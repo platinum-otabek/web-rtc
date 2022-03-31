6742bb159f0408613843550c5c06966cc4eb571b
platinum-otabek <mr.tursinboev@gmail.com>
2022-03-31 13:07:48 +0500

    qwe

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const server = require('https').Server(app)
+const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 const fs = require('fs');
